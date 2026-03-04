6d1b11b6c30d0aa2cf328924d3566a9b7de184bf
ChengHM00 <salinacheng0703@gmail.com>
2026-03-04 23:07:21 +0800

    Task2

---
+++ Task2 - Heap_HeapSort/HeapJob.py
@@ -0,0 +1,71 @@
+class MaxHeap:
+    def __init__(self):
+        self.data = []
+
+    def insert(self, value):
+        """Insert a value into the max-heap."""
+        self.data.append(value)
+        self._bubble_up(len(self.data) - 1)
+
+    def extract_max(self):
+        """Remove and return the maximum element."""
+        if not self.data:
+            return None
+        if len(self.data) == 1:
+            return self.data.pop()
+
+        max_value = self.data[0]
+        self.data[0] = self.data.pop()   # Move last element to root
+        self._bubble_down(0)
+        return max_value
+
+    def peek(self):
+        """Return max element without removing."""
+        return self.data[0] if self.data else None
+
+    # ---------- helpers ----------
+    def _bubble_up(self, index):
+        parent = (index - 1) // 2
+        while index > 0 and self.data[index] > self.data[parent]:
+            self.data[index], self.data[parent] = self.data[parent], self.data[index]
+            index = parent
+            parent = (index - 1) // 2
+
+    def _bubble_down(self, index):
+        size = len(self.data)
+        while True:
+            left = 2 * index + 1
+            right = 2 * index + 2
+            largest = index
+
+            if left < size and self.data[left] > self.data[largest]:
+                largest = left
+            if right < size and self.data[right] > self.data[largest]:
+                largest = right
+
+            if largest == index:
+                break
+
+            self.data[index], self.data[largest] = self.data[largest], self.data[index]
+            index = largest
+
+
+            # (priority, job_name)
+jobs = [
+    (2, "Job A"),
+    (5, "Job B"),
+    (3, "Job C"),
+    (1, "Job D")
+]
+
+heap = MaxHeap()
+
+# Insert all jobs
+for prio, name in jobs:
+    heap.insert((prio, name))
+
+# Extract in priority order
+print("Execution order:")
+while heap.peek() is not None:
+    prio, name = heap.extract_max()
+    print(f"{name} (priority {prio})")
---
+++ Task2 - Heap_HeapSort/README.md
@@ -0,0 +1,42 @@
+# RentEasy - Hotel and Homestay Management System
+<p align="center">
+    <img src="Assets/building_hotel.png" width="400">
+</p>
+
+
+<center> Hong Kong Metropolitan University
+
+Department of Electronic Engineering and Computer Science
+
+COMP8090SEF Data Structures And Algorithms 
+
+Final Project
+
+Cheng Hoi Man (11666130)
+</center>
+
+----------------------------------
+## Introduction
+
+This system aim at.........
+
+
+
+## Updates
+- 24 Feb 2026 - Draft of this document
+
+
+
+## Installation
+```shell
+git clone .....
+```
+
+## Quick Start
+
+
+
+
+
+## Contact
+For questions about code or paper, please email cheng.hm.73@gmail.com
